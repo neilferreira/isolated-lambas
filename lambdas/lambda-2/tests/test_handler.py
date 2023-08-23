d7ee155a1179512581f77358edcaa40c4d5cd3ee
Neil Ferreira <root@neilf.net>
2023-08-23 12:07:59 +0800

    Poetry conversion.

--- lambdas/lambda-2/tests/test_handler.py
+++
@@ -1,8 +0,0 @@
-def test_handler():
-    from handler import handler
-    assert handler(event={}).items() >= {"success": True}.items()
-
-
-def test_version_pin():
-    import pydicom
-    assert pydicom.__version__ == '2.4.0'
